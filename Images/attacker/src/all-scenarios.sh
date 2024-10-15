529a4251e67902493eb6b6fe265360af2e3cc50d
REDMOND\lleizerovich <lleizerovich@microsoft.com>
2024-10-15 18:15:25 +0300

    added version to installed packages in dockerfiles & changed some of the output in the scripts

--- Images/attacker/src/all-scenarios.sh
+++ Images/attacker/src/all-scenarios.sh
@@ -5,13 +5,13 @@
 
 # recon
 echo "--- Reconnaissance ---"
-echo "Checking read permissions for other pods via SelfSubjectAccessReview api request"
+echo "Checking read permissions for other pods via SelfSubjectAccessReview API request"
 kubetoken=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
 body='{"kind":"SelfSubjectAccessReview","apiVersion":"authorization.k8s.io/v1","metadata":{"creationTimestamp":null},"spec":{"resourceAttributes":{"namespace":"default","verb":"get","resource":"pods"}},"status":{}}'
 accessReview=`curl -s -k -A "kubectl" -H "Authorization: Bearer $kubetoken"  -H "Content-Type: application/json" -X POST -d $body "https://kubernetes.default/apis/authorization.k8s.io/v1/selfsubjectaccessreviews"| grep -Po '"status":\{\K.*?(?=\})'`
 echo "Results:  $accessReview"
 echo " "
-echo "Searching for pods listening on port 443 via Nmap: "
+echo "Searching for endpoints listening on port 443 via Nmap: "
 nmap  -Pn --open kubernetes.default/24 -p 443
 echo " "
 
